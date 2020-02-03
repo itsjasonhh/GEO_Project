80f3744069b991a5973ef896918bb565de427b51
jasonhuang <jahhuang@ucsc.edu>
2020-02-03 13:54:53 -0800

    Added intersection of line segments (checking for bounds as well), want to add functionality for 2 circles and segment and circle.

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import turtle
+import sys
 
 class Point:
     def __init__(self, x, y):
@@ -43,11 +44,41 @@ class Segment:
         turtle.penup()
         turtle.hideturtle()
 
+#returns two values: slope and y-intercept
+def get_equation(p1, p2):
+    if p1.x == p2.x and p1.y != p1.y:
+        return None, None
+    if p1.x == p2.x and p1.y == p2.y:
+        return None, None
+    else:
+        return (p1.y - p2.y)/(p1.x - p2.x), p1.y - p1.x*(p1.y - p2.y)/(p1.x - p2.x)
+
+def intersect(s1, s2):
+    if ((s1.point1.y - s1.point2.y)/(s1.point1.x - s1.point2.x) == (s2.point1.y - s2.point2.y)/(s2.point1.x - s2.point2.x)):
+        return None
+    else:
+        m1, b1 = get_equation(s1.point1,s1.point2)
+        m2, b2 = get_equation(s2.point1, s2.point2)
+        a = Point((b2-b1)/(m1-m2),m1*(b2-b1)/(m1-m2) + b1)
+        if not (a.x <= max(s1.point1.x,s1.point2.x) and a.x >= min(s1.point1.x,s1.point2.x)):
+            return None
+        if not (a.y <= max(s1.point1.y,s1.point2.y) and a.y >= min(s1.point1.y,s1.point2.y)):
+            return None
+        if not (a.x <= max(s2.point1.x,s2.point2.x) and a.x >= min(s2.point1.x,s2.point2.x)):
+            return None
+        if not (a.y <= max(s2.point1.y,s2.point2.y) and a.y >= min(s2.point1.y,s2.point2.y)):
+            return None
+        else:
+            return a
+
+
+
+
+
 def main():
-    p = Point(3, 10)
-    p1 = Point(100,100)
-    c = Segment(p,p1)
-    c.Draw()
+    s1 = Segment(Point(0,2),Point(2,4))
+    s2 = Segment(Point(5,-21),Point(2,-3))
+    print(intersect(s1,s2))
 
 
 
